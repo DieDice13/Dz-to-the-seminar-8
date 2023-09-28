ab10c5da9fff3b4a69ac6024e747beace38ecb01
Ruslan <die.dice.2003@mail.ru>
2023-09-29 02:34:19 +0600

    The third task is solved

---
+++ Task_3/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+int[,,] GetArray(int x, int y, int z)
+{
+    int[,,] array = new int[x, y, z];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = random.Next(1, 99);
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[,,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($"X({i}) Y({j}) ");
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        Console.Write( $"Z({k})={array[i,j,k]}; ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,,] array = GetArray(2, 2, 2);
+PrintArray(array);
