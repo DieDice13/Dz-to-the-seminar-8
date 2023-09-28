2e4b8eacbf670ba0b050594c9b7e97b6651b3cb1
Ruslan <die.dice.2003@mail.ru>
2023-09-29 01:49:30 +0600

    The first task i s solved

---
+++ Task_1/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.Clear();
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    Random random = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void ArrangeArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = GetArray(3, 4, 1, 99);
+PrintArray(array);
+ArrangeArray(array);
+System.Console.WriteLine();
+PrintArray(array);
+
